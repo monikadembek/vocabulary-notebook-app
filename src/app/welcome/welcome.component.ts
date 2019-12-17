6a5df6a9fc28618beff5c6ad79f159d8eb630ed0
Monika Dembek <monika.dembek@gmail.com>
2019-12-17 21:24:02 +0100

    create welcome component

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
+
+  user;
 
   ngOnInit() {
+    this.user = this.auth.user$.subscribe(data => this.user = data);
   }
 
 }
